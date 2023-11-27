4f02633cc3436f4ed69f47ffa07c149868ae16eb
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-11-27 12:04:49 -0500

    Agregado tooltip para cada TAB Elementos div sustituidos por a

--- src/components/DateRangeComponent.tsx
+++ src/components/DateRangeComponent.tsx
@@ -1,18 +1,20 @@
-import moment from 'moment';
-import { useState } from 'react';
 
-const DateRangeComponent = ({ranges, activeTab, handleTabClick, errorTab}) => {
+import { Tooltip } from 'react-tooltip';
+
+const DateRangeComponent = ({ ranges, activeTab, handleTabClick, errorTab }) => {
     return (
         <div className="flex flex-row justify-between bg-white">
             {Object.entries(ranges).map(([rangeId, range]) => (
-                <div
-                    key={rangeId}
-                    className={`p-3 hover:cursor-pointer w-full ${activeTab === rangeId ? ' bg-blue-400' : ''} ${errorTab === rangeId ? ' bg-red-400' : ''}`}
-                    onClick={() => handleTabClick(rangeId)}
-                >
+                <a data-tooltip-id={rangeId.toString()}
+                data-tooltip-content={`Desde ${range.toString()} hasta hoy`}
+                data-tooltip-place="top"
+                key={rangeId}
+                        className={`p-3 hover:cursor-pointer w-full ${activeTab === rangeId ? ' bg-blue-400' : ''} ${errorTab === rangeId ? ' bg-red-400' : ''}`}
+                        onClick={() => handleTabClick(rangeId)}>
+                    <Tooltip id={rangeId} />
                     {rangeId}
                     {/* {range[0].format('DD/MM/YYYY')} - {range[1].format('DD/MM/YYYY')} */}
-                </div>
+                </a>
             ))}
         </div>
     )
