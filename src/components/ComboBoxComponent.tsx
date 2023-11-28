a761966f6c23d3912fb3666049050d9bdf153cab
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-11-28 11:14:46 -0500

    Added labelText prop Added Dark mode support

--- src/components/ComboBoxComponent.tsx
+++ src/components/ComboBoxComponent.tsx
@@ -1,8 +1,8 @@
-export default function ComboBoxComponent ({ selectedOption, handleOptionChange, valuesMap, activeTab }) {
+export default function ComboBoxComponent ({ labelText, selectedOption, handleOptionChange, valuesMap, activeTab }) {
   return (
     <div className="relative m-2 flex">
-      <div className="m-auto">
-        <p>Mostrar cambio en</p> 
+      <div className="m-auto text-black dark:text-slate-400">
+        <p>{labelText}</p> 
       </div>
       <div className="inset-y-0 right-0 flex items-center ml-auto">
         <select
@@ -10,7 +10,7 @@ export default function ComboBoxComponent ({ selectedOption, handleOptionChange,
           name="currency"
           value={selectedOption}
             onChange={handleOptionChange}
-          className="h-full rounded-md border-0 bg-transparent py-2 pl-2 pr-7 text-gray-500 focus:ring-1 focus:ring-inset focus:ring-blue-600 sm:text-md"
+          className="h-full rounded-md border-0 bg-transparent py-2 pl-2 pr-7 text-gray-500 dark:text-slate-400 focus:ring-1 focus:ring-inset focus:ring-blue-600 sm:text-md"
         >
           {Object.entries(valuesMap).map(([tabId, tabText]) => (
           tabId !== activeTab ? (
