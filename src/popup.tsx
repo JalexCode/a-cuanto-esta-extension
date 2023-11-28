308395d8292fc549e69740b20915702d4e6a5621
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-11-28 09:25:50 -0500

    Added Page Nav state

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import CurrenciesPricesPage from './components/CurrenciesPricesPage';
+import AboutPage from './components/AboutPage'
 import './stylesheets/styles.css'
 
 function IndexPopup() {
+  const [currentPage, setCurrentPage] = useState("main")
+  const pages = {
+    main: <CurrenciesPricesPage handlePage={setCurrentPage} />,
+    about: <AboutPage handlePage={setCurrentPage} />
+  }
   return (
     <div className="App">
-      <CurrenciesPricesPage/>  
+      {pages[currentPage]}
     </div>
   );
 }
