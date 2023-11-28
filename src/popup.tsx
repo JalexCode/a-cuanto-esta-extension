3b93519d4916bc5808ac767d618b1e36f079c5e7
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-11-28 17:38:08 -0500

    Added persistent DarkMode

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CurrenciesPricesPage from './components/CurrenciesPricesPage';
 import AboutPage from './components/AboutPage'
 import './stylesheets/styles.css'
 
 function IndexPopup() {
+  // dark mode
+  const [darkMode, setDarkMode] = useState(false);
+  // pages
   const [currentPage, setCurrentPage] = useState("main")
   const pages = {
-    main: <CurrenciesPricesPage handlePage={setCurrentPage} />,
+    main: <CurrenciesPricesPage handlePage={setCurrentPage} darkMode={darkMode} setDarkMode={setDarkMode}/>,
     about: <AboutPage handlePage={setCurrentPage} />
   }
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
   return (
     <div className="App">
       {pages[currentPage]}
