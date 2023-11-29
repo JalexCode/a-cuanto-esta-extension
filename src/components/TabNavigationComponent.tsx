fa333d85608467ea769daa8cfd036b70d2aab17e
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-11-29 10:42:05 -0500

    Changes on Active Tabs in Night Mode

--- src/components/TabNavigationComponent.tsx
+++ src/components/TabNavigationComponent.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function TabNavigation({ tabsMap, activeTab, handleTabClick, selectedOption }) {
   return (
-    <div className="bg-blue-500 dark:bg-transparent justify-around dark:justify-between dark:m-auto text-white dark:text-slate-600 flex text-base">
+    <div className="bg-blue-500 dark:bg-transparent justify-around dark:justify-between dark:m-2 text-white dark:text-slate-600 flex text-base">
       {Object.entries(tabsMap).map(([tabId, tabText]) => (
         tabId !== selectedOption && tabId !== 'CUP' ? (
           <div
             key={tabId}
-            className={`p-3 hover:cursor-pointer w-full ${activeTab === tabId ? ' bg-blue-600 dark:bg-transparent dark:text-white ' : ''}`}
+            className={`p-3 dark:p-2 hover:cursor-pointer w-full dark:w-fit ${activeTab === tabId ? ' bg-blue-600 dark:bg-black dark:bg-opacity-20 dark:rounded-md dark:text-white ' : ''}`}
             // border-t-4 border-t-white
             onClick={() => handleTabClick(tabId)}
           >
