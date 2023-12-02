58646ec4eaf0be4754d92168755a9ef58fa42680
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-12-02 10:30:00 -0500

    Added icon provider Refactor imports

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,11 +1,10 @@
 import HeaderAboutComponent from "./HeaderAboutComponent";
-import logo from '../images/icon.png';
-import jalex from '../images/jalex.png';
-import jalexLight from '../images/jalex-light.png';
-import facebook from '../images/facebook.svg';
-import instagram from '../images/instagram.svg';
-import github from '../images/github.svg';
-
+import jalex from 'data-base64:~assets/images/jalex.png';
+import jalexLight from 'data-base64:~assets/images/jalex-light.png';
+import facebook from 'data-base64:~assets/images/facebook.svg';
+import instagram from 'data-base64:~assets/images/instagram.svg';
+import github from 'data-base64:~assets/images/github.svg';
+import icon from 'data-base64:~assets/images/icon.png'
 import { Tooltip } from "react-tooltip";
 
 const AboutPage = ({ handlePage, darkMode }) => {
@@ -14,12 +13,12 @@ const AboutPage = ({ handlePage, darkMode }) => {
             <HeaderAboutComponent handlePage={handlePage} />
             <div className="flex flex-col items-center p-6">
 
-                <img src={logo} alt="Logo" className="w-24 h-32" />
+                <img src={icon} alt="Logo" className="w-24 h-32" />
 
                 <p className="mt-2 text-lg font-bold text-black  dark:text-slate-600 dynamic-fill-color-2">¿A CUÁNTO ESTÁ?</p>
                 <p className='mt-2 text-xs text-slate-700 dark:text-slate-400'>Esta aplicación te mostrará el valor de cambio actual del Dólar, Euro y MLC en Cuba.</p>
-                <p className='text-xs text-slate-700 dark:text-slate-400'>Fuente de datos: <a className="text-blue-500 dark:text_white" href='https://eltoque.com/tasas-de-cambio-de-moneda-en-cuba-hoy' target='_blank' rel="noreferrer">eltoque.com</a></p>
-
+                <p className='mt-2 text-xs text-slate-700 dark:text-slate-400'>Fuente de datos: <a className="text-blue-500 dark:text_white" href='https://eltoque.com/tasas-de-cambio-de-moneda-en-cuba-hoy' target='_blank' rel="noreferrer">eltoque.com</a></p>
+                <span className="text-xs text-slate-700 dark:text-slate-400">Iconos de redes sociales: <a target="_blank" href="https://icons8.com" className="text-blue-500">Icons8</a></span>
                 <a id="clickable">
                     <Tooltip anchorSelect="#clickable" clickable>
                         <div className="flex flex-col">
@@ -35,6 +34,7 @@ const AboutPage = ({ handlePage, darkMode }) => {
                         <p className="text-sm text-black dark:text-slate-500 mr-2">Desarrollado por</p>
                         <img src={!darkMode ? jalexLight : jalex} alt="JalexCode Logo" className="w-24" />
                     </div>
+                    
                 </a>
 
             </div>
